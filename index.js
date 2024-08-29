6b2aa29ba61b007e987b94de345eab54f745ef8c
itiswen <77wen777@gmail.com>
2024-08-29 09:21:31 +0800

    Initital commitment

---
+++ index.js
@@ -0,0 +1,38 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+let likeCount = document.getElementsByClassName("like-count")
+const likeBtn = document.getElementById("like-btn")
+
+let likeCountSentence = `${posts[1].likes} likes`
+likeCount.textContent = likeCountSentence
+
+likeBtn.addEventListener("click", function like() {
+     posts[1].likes += 1
+})
